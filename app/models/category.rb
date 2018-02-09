33125f9bf1ba3007a8ec54f02c110ef5d5751715
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-09 19:45:58 -0300

    paranoia is working on category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-  belongs_to :user
-  has_many :products
+  acts_as_paranoid
+  # belongs_to :user
+  # has_many :products
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
-  validates :description, presence: true, length: { maximum: 25 } 
 end
