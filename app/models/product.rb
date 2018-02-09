918720d1bca1ac20943e3014d8d790d8c9d94c1d
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-09 19:11:42 -0300

    paranoia is working for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-  # belongs_to :category
+  # belongs_to :category # this was commented just to check if paperclip was working fine
+  acts_as_paranoid
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :name, presence: true, length: { minimum: 3, maximum: 50}
