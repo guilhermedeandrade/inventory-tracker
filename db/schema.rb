06aad5dec281375b169afb95ed423660d934b279
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-09 17:33:40 -0300

    added the password column for users and deleted the description column for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209010932) do
+ActiveRecord::Schema.define(version: 20180209203158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180209010932) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
