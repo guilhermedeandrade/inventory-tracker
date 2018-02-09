6de1d39aa764b6ffad707387d23f0e057ec8e5de
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-08 22:13:56 -0300

    generated the scaffolds

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+  belongs_to :user
   has_many :products
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates :description, presence: true, length: { maximum: 25 } 
