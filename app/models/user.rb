bbfe7a9340e05b8b719b964ba57643c4e9681d2f
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-10 02:45:59 -0300

    devise is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   acts_as_paranoid
   has_many :categories
   before_save { self.email = email.downcase }
-  validates :username, presence: true, uniqueness: { case_sensitive: false },
-  length: { minimum: 3, maximum: 25 }
-  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 25 },
-  uniqueness: { case_sensitive: false },
-  format: { with: VALID_EMAIL_REGEX }
+  # validates :username, presence: true, uniqueness: { case_sensitive: false },
+  # length: { minimum: 3, maximum: 25 }
+  # VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 25 },
+  # uniqueness: { case_sensitive: false },
+  # format: { with: VALID_EMAIL_REGEX }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
   resources :categories
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root 'pages#home'
+    
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+  devise_scope :user do
+    get '/logout', to: 'devise/sessions#destroy', as: :logout
+  end
 end
