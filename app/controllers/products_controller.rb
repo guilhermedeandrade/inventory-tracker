64f8dab4af8fda6ece9fad8e55bfe0523c1dc4a5
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-11 19:29:50 -0300

    my products session is working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-    # @product.user = current_user
+    @product.user = current_user
     @product.category = Category.first
     respond_to do |format|
       if @product.save
