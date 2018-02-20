fa943b5d21d2911618ee70ffc4daccf610e4a695
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-20 14:41:38 -0300

    ng-repeat is working with users

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -5,14 +5,13 @@ inventoryTracker = angular.module("inventoryTracker", [
 ]);
 
 angular.module("inventoryTracker").controller("usersList", function ($scope, $http) {
-  $scope.app = "Listing users";
-  console.log("testando");
+  $scope.app = "Testing AngularJS";
+  $scope.users = [];
 
   $http({
     method: 'GET',
     url: 'http://localhost:3000/users.json'
   }).then(function successCallback(response) {
-      console.log("test-success");
       $scope.users = response.data; // this is gonna access all the data from the JSON
       $scope.firstUser = response.data[0].email; // this is gonna access the email of the first user
     }, function errorCallback(response) {
