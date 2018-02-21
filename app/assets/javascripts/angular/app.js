e24b54a4cfb99ece514d6e9a2c34ce6c2a825c63
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-20 22:26:40 -0300

    changed some views using bootstrap

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -15,6 +15,32 @@ angular.module("inventoryTracker").controller("usersList", function ($scope, $ht
       $scope.users = response.data; // this is gonna access all the data from the JSON
       $scope.firstUser = response.data[0].email; // this is gonna access the email of the first user
     }, function errorCallback(response) {
-      console.log("test-error");
+      console.log("getUsers");
     });
+});
+
+angular.module("inventoryTracker").controller("categoriesList", function ($scope, $http) {
+  $scope.categories = [];
+
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/categories.json'
+  }).then(function successCallback(response) {
+    $scope.categories = response.data;
+  }, function errorCallback(response) {
+    console.log("getCategories");
+  });
+});
+
+angular.module("inventoryTracker").controller("productsList", function ($scope, $http) {
+  $scope.products = [];
+
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/products.json'
+  }).then(function successCallback(response) {
+    $scope.products = response.data;
+  }, function errorCallback(response) {
+    console.log("getProducts");
+  });
 });
