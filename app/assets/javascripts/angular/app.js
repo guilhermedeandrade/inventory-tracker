1c34a8bd71ad02b25d5431cbfab74629bf93e801
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-02-21 10:27:39 -0300

    changed the syntax for the users controller

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -4,27 +4,27 @@ inventoryTracker = angular.module("inventoryTracker", [
   'ngResource'
 ]);
 
-angular.module("inventoryTracker").controller("usersList", function ($scope, $http) {
+angular.module("inventoryTracker").controller("usersList", ["$scope", "$http", function ($scope, $http) {
   $scope.app = "Testing AngularJS";
   $scope.users = [];
 
   $http({
     method: 'GET',
-    url: 'http://localhost:3000/users.json'
+    url: 'users.json' //usando localhost nao vai funcionar com heroku
   }).then(function successCallback(response) {
       $scope.users = response.data; // this is gonna access all the data from the JSON
       $scope.firstUser = response.data[0].email; // this is gonna access the email of the first user
     }, function errorCallback(response) {
       console.log("getUsers");
     });
-});
+}]);
 
 angular.module("inventoryTracker").controller("categoriesList", function ($scope, $http) {
   $scope.categories = [];
 
   $http({
     method: 'GET',
-    url: 'http://localhost:3000/categories.json'
+    url: 'categories.json'
   }).then(function successCallback(response) {
     $scope.categories = response.data;
   }, function errorCallback(response) {
@@ -37,7 +37,7 @@ angular.module("inventoryTracker").controller("productsList", function ($scope, 
 
   $http({
     method: 'GET',
-    url: 'http://localhost:3000/products.json'
+    url: 'products.json'
   }).then(function successCallback(response) {
     $scope.products = response.data;
   }, function errorCallback(response) {
